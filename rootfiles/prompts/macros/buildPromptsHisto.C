ea66aa39df3c675cfca04e6a5b01b8d917d1b493
g-ricart <gabouric@gmail.com>
2022-06-09 16:00:11 +0200

    generate pions fonll histograms

--- rootfiles/prompts/macros/buildPromptsHisto.C
+++ rootfiles/prompts/macros/buildPromptsHisto.C
@@ -2,15 +2,21 @@
 #include "TH1F.h"
 #include "TString.h"
 
+TH1F* copyBinning(TH1F* hist_to_copy) {
+    Int_t nbins = hist_to_copy->GetNbinsX(); // Number of bins of histogram 1.
+    Double_t xbins[nbins+1]; // Array for low edges of histogram 1.
+    hist_to_copy->GetXaxis()->GetLowEdge(xbins);
+    // Must set last element of array as lower edge of overflow bin.
+    xbins[nbins] = hist_to_copy->GetXaxis()->GetBinLowEdge(nbins+1);
+
+    TH1F* hist = new TH1F("", "", nbins, xbins);
+    return hist;
+}
+
 TH1F* averageHist(TH1F* hist_1, TH1F* hist_2) {
 
-    Int_t nbins = hist_1->GetNbinsX(); // Number of bins of histogram 1.
-    Double_t xbins[nbins+1]; // Array for low edges of histogram 1.
-    hist_1->GetXaxis()->GetLowEdge(xbins);
-    // Must set last element of array as lower edge of overflow bin.
-    xbins[nbins] = hist_1->GetXaxis()->GetBinLowEdge(nbins+1);
-
-    TH1F* mean_hist = new TH1F("", "", nbins, xbins);
+    TH1F* mean_hist = copyBinning(hist_1);
+    Int_t nbins = mean_hist->GetNbinsX();
 
     for (size_t bin = 1; bin < nbins+1; bin++) {
         Float_t value_1 = hist_1->GetBinContent(bin);
@@ -38,7 +44,7 @@ void buildPromptsHisto() {
 
     // 0-10% centrality
     TH1F* eta_hist = averageHist(density_hist_1, density_hist_2);
-    eta_hist->SetName("eta");
+    eta_hist->SetNameTitle("eta", "pions, kaons and protons #eta");
 
     // Pions pT distribution file.
     TFile* pi_pt_f = TFile::Open("../rawData/ALICE_pi_pT.root");
@@ -52,11 +58,24 @@ void buildPromptsHisto() {
     pi_pt_f->Close();
 
     // 0-10% centrality
-    TH1F* pi_pt_h = averageHist(pi_pt_h_1, pi_pt_h_2);
-    pi_pt_h->SetName("pT");
+    TH1F* pi_pt_h_var = averageHist(pi_pt_h_1, pi_pt_h_2);
+    Int_t nbins = pi_pt_h_var->GetNbinsX();
+    TH1F* pi_pt_h     = new TH1F("pT", "pions p_{T}", 500, 0, 20);
 
-    // Save pion phase space histogram.
-    TFile* pion_ps_file = new TFile("../../fonll/LHCu5.root", "RECREATE");
+    // pT histogram has varaible binning -> Problems with RapidSim
+    for (size_t i = 0; i < 10000000; i++) {
+        Float_t rnd_value = pi_pt_h_var->GetRandom();
+        Int_t bin = pi_pt_h_var->FindBin(rnd_value);
+        Float_t bin_width = pi_pt_h_var->GetXaxis()->GetBinWidth(bin);
+
+        pi_pt_h->Fill(rnd_value, bin_width);
+    }
+
+    pi_pt_h->Draw();
+
+
+    // Save pion phase space histograms.
+    TFile* pion_ps_file = new TFile("../../fonll/LHCpi5.root", "RECREATE");
     eta_hist->Write();
     pi_pt_h->Write();
 
